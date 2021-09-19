9320631aafd6803141d078886202c5f7ff0b8810
irmsonmz <irmsonmz@gmail.com>
2021-09-19 15:10:56 +0300

    Choose game type

---
+++ guessTheNumber.py
@@ -0,0 +1,5 @@
+print("*****WELCOME THE GAME*****")
+game_type = None
+while game_type not in ["1","2"]:
+    print("1.Computer \n2.User")
+    game_type = input("Please choose the game type : (Eg. :1,2)\n")
